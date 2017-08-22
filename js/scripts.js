b4355d92683a26a2f1e56530b7e0fc3637b8a6fb
Gennaro Anzalone and Rafael Furry <gennaro+rafael@>
2017-08-22 13:53:41 -0700

    Add final touches with table display

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,43 @@
-function Ticket(movie, age, time) {
+function Ticket(movie, age, time, price) {
   this.movie = movie;
   this.age = age;
   this.time = time;
+  this.price = price;
  }
 
+function formula(ticket) {
+  if(ticket.age == "Youth"){
+    ticket.price -= 2;
+  } else if(ticket.age == "Senior"){
+    ticket.price -= 3;
+  }
 
-$(document).ready(function() {
-  $('#submit').click(function() {
+  if(ticket.time == "10:00 am"){
+    ticket.price -= 3;
+  } else if(ticket.time == "12:00 pm"){
+    ticket.price -= 2;
+  }
+  return ticket.price;
+}
+
+
+$(document).ready(function(){
+  $('#submit').click(function(event) {
+    event.preventDefault();
 
     var movie = $("#movie option:selected").text();
     var age = $("#age option:selected").text();
     var time = $("#time option:selected").text();
+    var price = 10;
+    var newTicket = new Ticket(movie, age, time, price);
 
-    var newTicket = new Ticket(movie, age, time);
-    alert(newTicket.time);
+    price = formula(newTicket);
+
+    $("#movieOut").text(newTicket.movie);
+    $("#ageOut").text(newTicket.age);
+    $("#timeOut").text(newTicket.time);
+    $("#priceOut").text(price);
+
+    $(".table").show()
   });
-
-
-
-
 });
